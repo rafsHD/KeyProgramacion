08b04833001edeafc59c7a66e1090d2d88eb216a
Rafa Santos <>
2025-05-29 22:46:10 -0600

    Update ejercicios2.py

--- ejercicios2.py
+++ ejercicios2.py
@@ -1,15 +1,15 @@
 '''
 Clase:        Fortalecimiento lógico clase 3
 Tema:         Fortalecimiento lógico 
-Ejercicio:    Ver el total a pagar de una cuenta, creacion de correo institucional, ver si la contrasña es segura, calculadora de impuestos y numero magico 
-Descripción:  Contraseña, Calculadora de impuestos y numero magico 
+Ejercicio:    5.4.1 y 5.4.2
+Descripción:  Numero secreto y Sumador de valores posicionales
 
 Autor:        Jose Rafael Santos Reyes
 Fecha:        2025-05-16
 Estado:       [Terminado]   
 '''
 import random
-
+'''
 print("Ejercicio 5.4.1")
 
 n = random.randint(1,100)
@@ -26,6 +26,21 @@ while acierto:
         print(f"Felicidades! Has encontrado el numero secreto! ({n})")
         acierto == False
         break
+        '''
+print("Ejercicio 5.4.2")
+
+numero = input(("Esribe un numero al azar: "))      
+
+for i in numero:
+    i = int(i)
+    i += [i] + 1
+    
+print(i)
         
+
+    
+    
+    
+
 
 
